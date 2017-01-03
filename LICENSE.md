c3ada316801f0dd593827551072ac1293b62f825
dpfaffenbauer <dominik@lineofcode.at>
2017-01-03 16:47:00 +0100

    update header for new copyright

--- LICENSE.md
+++ LICENSE.md
@@ -1,5 +1,5 @@
 # License
-Copyright (C) 2016 Dominik Pfaffenbauer
+Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
--- controllers/Admin/ProcessController.php
+++ controllers/Admin/ProcessController.php
@@ -8,7 +8,7 @@
  * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
  * files that are distributed with this source code.
  *
- * @copyright  Copyright (c) 2016 lineofcode.at (http://www.lineofcode.at)
+ * @copyright  Copyright (c) 2015-2017 Dominik Pfaffenbauer (https://www.pfaffenbauer.at)
  * @license    https://github.com/dpfaffenbauer/ProcessManager/blob/master/gpl-3.0.txt GNU General Public License version 3 (GPLv3)
  */
 
