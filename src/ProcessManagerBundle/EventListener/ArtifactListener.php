6858facc7fb309971a7d887d5ed06b8dfe6bf94a
Dalibor Karlović <dalibor.karlovic@flexolabs.io>
2018-10-24 11:39:29 +0200

    feature: artifact support, nullify artifact if asset is deleted

--- src/ProcessManagerBundle/EventListener/ArtifactListener.php
+++ src/ProcessManagerBundle/EventListener/ArtifactListener.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace ProcessManagerBundle\EventListener;
 
+use Pimcore\Event\Model\AssetEvent;
 use Pimcore\File as FileHelper;
 use Pimcore\Model\Asset;
+use ProcessManagerBundle\Model\Process;
 use ProcessManagerBundle\Model\ProcessInterface;
 use ProcessManagerBundle\Monolog\ProcessLogEvent;
 
@@ -14,6 +16,8 @@ use ProcessManagerBundle\Monolog\ProcessLogEvent;
  */
 class ArtifactListener
 {
+    private const METADATA_MARKER = 'process_manager.process';
+
     /**
      * @param ImportDefinitionEvent $event
      */
@@ -42,12 +46,29 @@ class ArtifactListener
         // TODO: name the file something like "<executable name> artifact <current datetime>" or provide an artifact name template in the Executable config
         $artifact->setFilename(pathinfo($artifactPath, PATHINFO_FILENAME));
         // TODO: add an event listener which sets Artifact=null (or deletes the Process) if the user deletes this asset
-        $artifact->addMetadata('process_manager.process', 'number', $process->getId());
+        $artifact->addMetadata(self::METADATA_MARKER, 'number', $process->getId());
         $artifact->save();
 
         fclose($fileHandle);
 
         $process->setArtifact($artifact);
+        $process->save();
+    }
+
+    public function onArtifactAssetDelete(AssetEvent $event)
+    {
+        $asset = $event->getAsset();
+
+        $id = $asset->getMetadata(self::METADATA_MARKER);
+        if (empty($id)) {
+            return;
+        }
+
+        $process = Process::getById($id);
+        if (null === $process) {
+            return;
+        }
+        $process->setArtifact(null);
         $process->save();
     }
 }
